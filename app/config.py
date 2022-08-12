0411d79b3d397174f97458869edcab4a2506d4f6
bjogden <bjogden123@gmail.com>
2022-08-12 17:26:12 -0500

    make global config vars uppercase

--- app/config.py
+++ app/config.py
@@ -2,10 +2,10 @@ from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 
 
-options = Options()
-options.add_argument("--headless")
+OPTIONS = Options()
+OPTIONS.add_argument("--headless")
 
-service = Service(
+SERVICE = Service(
     executable_path='/usr/bin/geckodriver',
     log_path='logs/geckodriver.log',
 )
